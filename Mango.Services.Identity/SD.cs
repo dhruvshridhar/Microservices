8cd3a3c5ab754c505c50b103296b937f9eda3c18
Dhruv Shridhar <dhruvshridhar@gmail.com>
2022-09-22 21:08:57 +0530

    Changes in SD

--- Mango.Services.Identity/SD.cs
+++ Mango.Services.Identity/SD.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using Duende.IdentityServer.Models;
+
 namespace Mango.Services.Identity
 {
-    public class SD
+    public static class SD
     {
-        public SD()
-        {
-        }
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("Mango","Mango Server"),
+                new ApiScope(name:"read", displayName:"Read your data"),
+                new ApiScope(name:"write",displayName:"Write your data"),
+                new ApiScope(name:"delete",displayName:"Delete your data")
+            };
     }
 }
 
